a58b7040debfd03de4df0b870d7d9a75bf6917e2
hmedo <hmedo@7medo>
2024-09-05 02:05:34 +0300

    v1_02-05

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,7 +10,6 @@ class Auth:
     """
     Class Auth for handling authentication
     """
-
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
         method which returns false
