6acef3802a73f711f2ffc89820495c7b7fcccd81
hmedo <hmedo@7medo>
2024-09-05 02:01:15 +0300

    v1_02-01

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Module which defines an auth class
+"""
+from flask import request
+from typing import List
+
+
+class Auth:
+    """
+    Class Auth for handling authentication
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        method which returns false
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        method which returns None
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        method which returns None
+        """
+        return None
