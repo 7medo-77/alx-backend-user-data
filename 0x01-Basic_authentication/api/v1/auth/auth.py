72f999ea2f561ad900d520c051a221b11752dafd
hmedo <hmedo@7medo>
2024-09-05 20:30:42 +0300

    main_1.py_20-30

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -4,6 +4,7 @@ Module which defines an auth class
 """
 from flask import request
 from typing import List, TypeVar
+import re
 
 
 class Auth:
@@ -13,7 +14,19 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ method which returns false
         """
-        return False
+        excluded_paths_string_array = []
+        if not path or not excluded_paths or len(excluded_paths) == 0:
+            return True
+        else:
+            path_strings = re.findall(r'(\w+)+/?', path)
+            for path in excluded_paths:
+                excluded_paths_string_array.append(
+                    re.findall(r'(\w+)+/?', path)
+                )
+            if path_strings in excluded_paths_string_array:
+                return False
+            else:
+                return True
 
     def authorization_header(self, request=None) -> str:
         """ method which returns None
