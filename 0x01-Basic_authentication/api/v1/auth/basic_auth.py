14bae826dac1604a92a12deb7d28c134083ceb00
hmedo <hmedo@7medo>
2024-09-06 18:02:19 +0300

    main_3.py_18-02

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,7 @@ Module which defines a basic auth class
 from flask import request
 from typing import List, TypeVar
 import base64
+import binascii
 from api.v1.auth.auth import Auth
 
 
@@ -25,3 +26,21 @@ class BasicAuth(Auth):
         else:
             auth_header_binary = authorization_header[6:]
             return auth_header_binary
+
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str
+        ) -> str:
+        """
+        Method to decode base64 authorization header from a request
+        """
+        if not base64_authorization_header or\
+            not isinstance(base64_authorization_header, str):
+            return None
+        else:
+            try:
+                binary_auth_header = base64_authorization_header.encode('utf-8')
+                return base64.b64decode(binary_auth_header).decode('utf-8')
+            except binascii.Error:
+                return None
