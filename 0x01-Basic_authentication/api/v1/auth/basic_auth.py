ebf9431e710703e24312ab0c6ad6fdf85120c305
hmedo <hmedo@7medo>
2024-09-06 17:09:28 +0300

    main_2.py_17-09

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 """
-Module which defines an auth class
+Module which defines a basic auth class
 """
 from flask import request
 from typing import List, TypeVar
-import re
+import base64
 from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
     """ Class BasicAuth
     """
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """
+        Method to extract base64 authorization header from a request
+        """
+        if not authorization_header or\
+            not isinstance(authorization_header, str) or\
+            authorization_header[:6] != 'Basic ':
+            return None
+        else:
+            auth_header_binary = authorization_header[6:]
+            return auth_header_binary
