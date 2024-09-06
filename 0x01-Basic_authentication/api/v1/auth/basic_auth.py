b7f6c8fe028d0a3a2628eeae55adbaadd37b89c9
hmedo <hmedo@7medo>
2024-09-06 17:10:39 +0300

    main_2.py_17-10

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -12,13 +12,15 @@ class BasicAuth(Auth):
     """ Class BasicAuth
     """
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str
+                                            ) -> str:
         """
         Method to extract base64 authorization header from a request
         """
         if not authorization_header or\
-            not isinstance(authorization_header, str) or\
-            authorization_header[:6] != 'Basic ':
+                not isinstance(authorization_header, str) or\
+                authorization_header[:6] != 'Basic ':
             return None
         else:
             auth_header_binary = authorization_header[6:]
