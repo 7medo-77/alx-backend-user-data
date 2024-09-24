7e54ee0afd69e59bc56995f43acb87be1a368655
hmedo <hmedo@7medo>
2024-09-25 01:47:17 +0300

    a.db_01-47

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -23,30 +23,27 @@ class Auth:
 
     def __init__(self):
         self._db = DB()
+        self._db._session
 
-    def register_user(self, email: str, password: str) -> User:
-        """ Registers a user in the database
-        Returns: User Object
-        """
-
-        try:
-            user = self._db.find_user_by(email=email)
-        except NoResultFound:
-            hashed_password = _hash_password(password)
-            user = self._db.add_user(email, hashed_password)
-
-            return user
-
-        else:
-            raise ValueError(f'User {email} already exists')
-
-    # def register_user(self, email: str, password: str) -> TypeVar('User'):
-    #     """ Method which returns a user object """
-    #     # self._db._session
+    # def register_user(self, email: str, password: str) -> User:
+    #     """ Registers a user in the database
+    #     Returns: User Object
+    #     """
     #     try:
-    #         user_exists = self._db.find_user_by(email=email)
-    #         if user_exists:
-    #             raise ValueError('User {} already exists'.format(email))
+    #         user = self._db.find_user_by(email=email)
     #     except NoResultFound:
     #         hashed_password = _hash_password(password)
-    #         return self._db.add_user(email, hashed_password)
+    #         user = self._db.add_user(email, hashed_password)
+    #         return user
+    #     else:
+    #         raise ValueError(f'User {email} already exists')
+
+    def register_user(self, email: str, password: str) -> User:
+        """ Method which returns a user object """
+        try:
+            user_exists = self._db.find_user_by(email=email)
+            if user_exists:
+                raise ValueError('User {} already exists'.format(email))
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            return self._db.add_user(email, hashed_password)
