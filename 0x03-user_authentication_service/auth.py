5dc23141a7608681ddf8d11e11d5e35514170ab6
hmedo <hmedo@7medo>
2024-09-25 00:58:38 +0300

    auth.py_00-58

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,13 +3,10 @@
 Module which defines authentication methods
 """
 import bcrypt
-from typing import ByteString
-
-
-# def _hash_password(password: str) -> str:
-#     """ Returns a salted hash of the input password """
-#     hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
-#     return hashed
+from typing import ByteString, TypeVar
+from db import DB
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 
 def _hash_password(password: str) -> str:
@@ -17,3 +14,20 @@ def _hash_password(password: str) -> str:
     Method which returns a salted hash of a password
     """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> TypeVar('User'):
+        """ Method which returns a user object """
+        try:
+            user_exists = self._db.find_user_by(email=email)
+            if user_exists:
+                raise ValueError('User {} already exists'.format(email))
+        except NoResultFound:
+            hashed_password = str(_hash_password(password))
+            return self._db.add_user(email, hashed_password)
