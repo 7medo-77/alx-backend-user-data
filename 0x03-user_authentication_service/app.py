96d1033e78b4cee25d52b6d3dbf5093eb02ff829
hmedo <hmedo@7medo>
2024-09-25 15:28:43 +0300

    app.py_15-28

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -7,6 +7,7 @@ from flask import Flask, json
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'], strict_slashes=False)
 def simple_return():
     """
@@ -14,6 +15,7 @@ def simple_return():
     """
     return_message = {"message": "Bienvenue"}
     return json.jsonify(return_message)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
