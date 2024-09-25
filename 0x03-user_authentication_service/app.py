2386fae1f73fd7602a80865c08ac2fa873665693
hmedo <hmedo@7medo>
2024-09-25 15:27:33 +0300

    app.py_15-27

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Simple flask application
+"""
+from flask import Flask, json
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def simple_return():
+    """
+    Return a simple json dictionary
+    """
+    return_message = {"message": "Bienvenue"}
+    return json.jsonify(return_message)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
