a051fbde081084f4c2ff1463e9a1522ccb816cc2
hmedo <hmedo@7medo>
2024-09-25 15:49:57 +0300

    a.db_15-49

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,10 +2,12 @@
 """
 Simple flask application
 """
-from flask import Flask, json
+from flask import Flask, json, request
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
@@ -17,5 +19,24 @@ def simple_return():
     return json.jsonify(return_message)
 
 
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    """
+    POST method which implements adding a new user
+    """
+    email = request.form['email']
+    password = request.form['password']
+    try:
+        AUTH.register_user(email, password)
+        return json.jsonify({
+            'email': email,
+            'message': 'user created'
+        })
+    except ValueError:
+        return json.jsonify({
+            'message': 'email already registered'
+        }), 400
+
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port='5000', debug=True)
